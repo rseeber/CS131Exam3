dbe3e589199a64a9e683dfe112ca2ed34847ef70
River Seeber <riverseeber12@gmail.com>
2024-11-13 11:50:00 -0800

    fix main method

--- exam3/Main.java
+++ exam3/Main.java
@@ -26,6 +26,68 @@ class Main {
 
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
+        Cipher cipher = new Cipher(3);
+        String output;
+        String passwd = "";
+        String checksum = "";
+
+        //get the protocol
+        System.out.print("Pick an option:\n[1] Caeser Cipher\n[2] ROT13\n> ");
+        int protocol = scnr.nextInt();
+        
+        //forward or backwards?
+        System.out.print("Do you want to encrypt a message, or decrypt?\n[1] Encrypt\n[2] Decrypt\n > ");
+        int encryption = scnr.nextInt();
+        
+        //do checksum?
+        System.out.print("Do checksum?\n[1] Yes\n[2] No\n> ");
+        int doChecksum = scnr.nextInt();
+        scnr.nextLine();
+        
+        //get message
+        System.out.print("Paste input message:\n> ");
+        String msg = scnr.nextLine();
+
+        if (doChecksum == 1){
+            //get password
+            System.out.print("Paste shared secret (password)\n> ");
+            passwd = scnr.nextLine();
+            //get checksum if we're decrypting
+            if(encryption == 2){
+                System.out.print("Paste checksum from transmission\n> ");
+                checksum = scnr.nextLine();
+            }
+        }
+
+        //if protocol is caeser
+        if(protocol == 1){
+            if(encryption == 1){
+                output = cipher.caeserCipher(msg);
+                System.out.println("[Encrypted Message]:\n"+output+"\n\n");
+                if(doChecksum == 1){
+                    checksum = cipher.getChecksum(msg, passwd);
+                    System.out.println("[Checksum]:\n"+checksum);
+                }
+            }
+            else{
+                output = cipher.undoCaeser(msg);
+                boolean secure = true;
+                if(doChecksum == 1){
+                    secure = cipher.verifyChecksum(output, passwd, checksum);
+                }
+                if(secure){
+                    System.out.println("\n[Decrypted Message]:\n" + output + "\n\n");
+                    System.out.println("Checksum matches message + password combination. Your transmission is from who you think it's from.\n");
+                }
+                else{
+                    System.out.println("CHECKSUM DOESN'T MATCH MESSAGE + PASSWORD COMBINATION. ABORTING\n");
+                }
+            }
+        }
+    }
+
+    public static void yea (){
+        Scanner scnr = new Scanner(System.in);
         //creates a new cipher object, with jump value of 3
         Cipher cipher = new Cipher(3);
         System.out.println("Pick message to encrypt:\n[1] \"Hello, World\"\n[2] JFK's 'We Choose To Go To The Moon' Speech\n");
